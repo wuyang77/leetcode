056c20f4624b9f51717e8c6ff90c751baa5a02a0
wuyang <2388958622@qq.com>
2024-12-15 01:24:28 +0800

    初始化

---
+++ 59.螺旋矩阵-ii.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=59 lang=java
+ *
+ * [59] 螺旋矩阵 II 边界的处理问题
+ */
+
+// @lc code=start
+
+import java.util.Arrays;
+
+class Solution {
+    public int[][] generateMatrix(int n) {
+        // l左边，r右边，t顶部，b底部
+        int l = 0, r = n - 1, t = 0, b = n -1; 
+        // 创建一个n维的矩阵
+        int[][] mat = new int[n][n]; 
+        // num为迭代初始值，tar为迭代终值
+        int num = 1, tar = n * n; 
+        while (num <= tar) {
+            for (int i = l; i <= r; i++) {mat[t][i] = num++;}
+            t++;
+            for (int i = t; i <= b; i++) {mat[i][r] = num++;}
+            r--;
+            for (int i = r; i >= l; i--) {mat[b][i] = num++;}
+            b--;
+            for (int i = b; i >= t; i--) {mat[i][l] = num++;}
+            l++;
+        }
+        return mat;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(Arrays.deepToString(solution.generateMatrix(10)));
+    }
+}
+// @lc code=end
+
