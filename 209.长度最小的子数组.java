df163c2c30459fd2ba707cec6e63989346e50979
wuyang77 <2388958622@qq.com>
2024-12-31 16:53:19 +0800

    长度最小子数组

---
+++ 209.长度最小的子数组.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=209 lang=java
+ *
+ * [209] 长度最小的子数组
+ * 给定一个含有 n 个正整数的数组和一个正整数 target 。
+ * 找出该数组中满足其总和大于等于target 的长度最小的子数组[numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。
+ * 如果不存在符合条件的子数组，返回 0 。
+ */
+
+// @lc code=start
+class Solution209 {
+    public int minSubArrayLen(int target, int[] nums) {
+        int left = 0, sum = 0, result = Integer.MAX_VALUE;
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            while(sum >= target) {
+                result = Math.min(result, right - left + 1);
+                sum -= nums[left++];
+            }
+        }
+        return result == Integer.MAX_VALUE ? 0 : result;      
+    }
+    public static void main(String[] args) {
+        Solution209 solution209 = new Solution209();
+        int[] newNums = {2,3,1,2,4,3};
+        int minSubArrayLen = solution209.minSubArrayLen(7, newNums);
+        System.out.println(minSubArrayLen);
+    }
+}
+// @lc code=end
+
