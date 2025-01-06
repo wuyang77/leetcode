c0224314d0213bfc7b31075e90303b6c4504915e
wuyang77 <2388958622@qq.com>
2025-01-06 10:22:35 +0800

    移除链表元素

---
+++ 链表/203.移除链表元素.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=java
+ *
+ * [203] 移除链表元素
+ */
+
+// @lc code=start
+
+import 链表.定义链表节点.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        while(head!=null && head.val==val) {
+            head = head.next;
+        }
+        ListNode curr = head;
+        while(curr!=null && curr.next !=null) {
+            if (curr.next.val == val) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
+        }
+        return head;
+    }
+}
+// @lc code=end
+
