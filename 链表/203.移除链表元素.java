e6f735e17b3fe8415f87701301c51a8030a16ad3
wuyang77 <2388958622@qq.com>
2025-01-08 10:17:23 +0800

    1月8号用原来的链表操作（最终修订）

--- 链表/203.移除链表元素.java
+++ 链表/203.移除链表元素.java
@@ -23,7 +23,7 @@ import 定义链表节点.ListNode;
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution203 {
+class Solution {
     public ListNode removeElements(ListNode head, int val) {
         // 1.用原来的链表操作：
         while(head != null && head.val == val) {
@@ -55,26 +55,26 @@ class Solution203 {
     }
 
     
-    public static void main(String[] args) {
-        ListNode node1 = new ListNode(6);
-        ListNode node2 = new ListNode(2);
-        ListNode node3 = new ListNode(6);
-        ListNode node4 = new ListNode(3);
-        ListNode node5 = new ListNode(4);
-        ListNode node6 = new ListNode(5);
-        ListNode node7 = new ListNode(6);
-        node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        node4.next = node5;
-        node5.next = node6;
-        node6.next = node7;
-        node7.next = null;
+    // public static void main(String[] args) {
+    //     ListNode node1 = new ListNode(6);
+    //     ListNode node2 = new ListNode(2);
+    //     ListNode node3 = new ListNode(6);
+    //     ListNode node4 = new ListNode(3);
+    //     ListNode node5 = new ListNode(4);
+    //     ListNode node6 = new ListNode(5);
+    //     ListNode node7 = new ListNode(6);
+    //     node1.next = node2;
+    //     node2.next = node3;
+    //     node3.next = node4;
+    //     node4.next = node5;
+    //     node5.next = node6;
+    //     node6.next = node7;
+    //     node7.next = null;
         
-        Solution203 solution203 = new Solution203();
-        System.out.println(solution203.removeElements(node1, 6).toString());
+    //     Solution203 solution203 = new Solution203();
+    //     System.out.println(solution203.removeElements(node1, 6).toString());
         
-    }
+    // }
 }
 // @lc code=end
 
