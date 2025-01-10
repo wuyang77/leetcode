793fc25d89d0b36769f04a3700ff2a6e3fd1686d
wuyang77 <2388958622@qq.com>
2025-01-10 17:06:29 +0800

    更新双链表的查功能

--- 链表/707.设计链表.java
+++ 链表/707.设计链表.java
@@ -35,56 +35,72 @@ class MyLinkedList{
     }
     
     public int get(int index) {
-        if(index < 0 || index > size) {
+        // 如果索引无效，返回 -1
+        if(index < 0 || index >= size) {
             return -1;
         }
+        // 遍历链表，找到第 index 个节点
         ListNode cur = head;
         for (int i = 0; i < index; i++) {
             cur = cur.next;
         }
+        // 返回第 index 个节点的值
         return cur.val;
     }
     
     public void addAtHead(int val) {
         ListNode newNode = new ListNode(val);
+        // 将新节点插入到头结点之后
         newNode.next = head.next;
         head.next = newNode;
+        // 链表大小加 1
         size++;
     }
     
     public void addAtTail(int val) {
         ListNode newNode = new ListNode(val);
+        // 遍历链表，找到最后一个节点
         ListNode cur = head;
         while(cur.next != null) {
             cur = cur.next;
         }
+        // 将新节点插入到最后一个节点之后
         cur.next = newNode;
+        // 链表大小加 1
         size++;
      }
     
     public void addAtIndex(int index, int val) {
-        if(index < 0 || index > size) {
+        // 如果索引无效，直接返回
+        if(index < 0 || index >= size) {
             return;
         }
+        // 遍历链表，找到第 index 个节点
         ListNode cur = head;
         ListNode newNode = new ListNode(val);
         for (int i = 0; i < index; i++) {
             cur = cur.next;
         }
+        // 将新节点插入到第 index 个节点之后
         newNode.next = cur.next;
         cur.next = newNode;
+        // 链表大小加 1
         size++;
     }
     
     public void deleteAtIndex(int index) {
-        if(index < 0 || index > size) {
+        // 如果索引无效，直接返回
+        if(index < 0 || index >= size) {
             return;
         }
+        // 遍历链表，找到第 index 个节点
         ListNode cur = head;
         for (int i = 0; i < index; i++) {
             cur = cur.next;
         }
+        // 删除第 index 个节点
         cur.next = cur.next.next;
+        // 链表大小减 1
         size--;
     }
     public static void main(String[] args) {
@@ -95,6 +111,65 @@ class MyLinkedList{
         obj.get(1);
         obj.deleteAtIndex(1);
         obj.get(1);       
+    }
+}
+/**
+ * 设计链表双链表的解法
+ 
+ */
+class MyLinkedList2 {
+    
+    class ListNode {
+        int val;
+        ListNode next, prev;
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+    /** 记录的是链表的大小 */
+    private int size;
+    /** 记录的是虚拟头结点和虚拟尾结点 */
+    private ListNode head, tail;
+
+    /** 用于判断从头或尾遍历的阈值 */
+    private static final int HALF = 2;
+
+    /**
+     * 初始化链表
+     * 初始化虚拟头结点和虚拟尾结点
+     * 将虚拟头结点和虚拟尾结点连接起来
+     * 初始化链表大小
+     */
+    public MyLinkedList2() {
+        this.size = 0;
+        this.head = new ListNode(0);
+        this.tail = new ListNode(0);
+        this.head.next = tail;
+        this.tail.prev = head;
+    }
+    
+    public int get(int index) {
+        if(index < 0 || index >= size) {
+            return -1;
+        }
+        // 用于遍历链表的节点
+        ListNode cur;
+        // 判断从头遍历还是从尾遍历，哪边遍历的时间更短
+        if(index < size / HALF) {
+            cur = head;
+            for (int i = 0; i < index; i++) {
+                // 从头开始遍历
+               cur = cur.next; 
+            }
+        } else {
+            cur = tail;
+            for (int i = 0; i < size - index; i++) {
+                // 从尾开始遍历
+                cur = cur.prev;
+            }
+        }
+        // 返回第 index 个节点的值
+        return cur.val;
     }
 }
 
